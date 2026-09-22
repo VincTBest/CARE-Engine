f71a353f44cc34aadcf99dc586da3ed749c87713
VincTBest <149181053+VincTBest@users.noreply.github.com>
2026-09-22 19:16:54 +0200

    Added Picotron Userdata-like 2D arrays.

---
+++ util/2d_array.lua
@@ -0,0 +1,50 @@
+
+function create2DArray(w, h, defaultEl)
+    -- Basically userdata from Picotron.
+    defaultEl = defaultEl or 0
+
+    local array = {
+        data = {},
+        w = w,
+        h = h,
+    }
+
+    for i = 1, h do
+        local row = {}
+        for j = i, w do
+            table.insert(row, { defaultEl })
+        end
+        table.insert(array.data, row)
+    end
+
+    function array.get(x, y)
+        local el = array.data[y][x]
+        return el
+    end
+
+    function array.set(x, y, v)
+        array.data[y][x] = v
+    end
+
+    function array.forEach(func)
+        for y = 1, array.h do
+            for x = 1, array.w do
+                local el = array.data[y][x]
+                array.data[y][x] = func(el, x, y)
+            end
+        end
+    end
+
+    function array.arith(exp, v)
+        array.forEach(function(ov, _x, _y) return exp(ov, v) end)
+    end
+
+    return array
+end
+
+local ARITH = {
+    ADD = function(o, v) return o+v end,
+    SUB = function(o, v) return o-v end,
+    MUL = function(o, v) return o*v end,
+    DIV = function(o, v) return o/v end,
+}
